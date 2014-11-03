f8f3faa7614eae7b671ff741523a2942c2184f77
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2014-11-03 11:12:37 +0100

    Q2

--- cojez_tp5.sql
+++ cojez_tp5.sql
@@ -3,4 +3,7 @@
 SELECT fid FROM Films
 EXCEPT
 SELECT DISTINCT film FROM Prix;
+--E1Q2
+SELECT DISTINCT film AS fid, artiste AS realisateur FROM Prix
+WHERE artiste IN (SELECT realisateur FROM Films);
 
