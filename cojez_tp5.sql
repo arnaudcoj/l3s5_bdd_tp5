84ee67d5765e9fa60508443a2cd2fda920bb5d0e
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2014-11-03 11:47:41 +0100

    q2 refaite

--- cojez_tp5.sql
+++ cojez_tp5.sql
@@ -6,4 +6,7 @@ SELECT DISTINCT film FROM Prix;
 --E1Q2
 SELECT DISTINCT film AS fid, artiste AS realisateur FROM Prix
 WHERE artiste IN (SELECT realisateur FROM Films);
+--E1Q3
+INSERT INTO Prix
+ VALUES (4,'meilleur gagnant de prix de meilleur acteur',2,123,'2012-12-12');
 
