dde52eb178e07ac0ed347bd79b5b64171cf7ebb0
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2014-11-05 00:44:31 +0100

    Q2 !!

--- cojez_tp5.sql
+++ cojez_tp5.sql
@@ -5,8 +5,13 @@ EXCEPT
 SELECT DISTINCT film FROM Prix;
 --E1Q2
 SELECT DISTINCT film AS fid, artiste AS realisateur FROM Prix
-WHERE artiste IN (SELECT realisateur FROM Films);
+WHERE artiste IN (SELECT DISTINCT realisateur FROM Films) AND NOT film IN (
+SELECT DISTINCT film FROM Prix
+WHERE artiste IN (SELECT DISTINCT aid FROM Roles));
 --E1Q3
 INSERT INTO Prix
  VALUES (4,'meilleur gagnant de prix de meilleur acteur',2,123,'2012-12-12');
 
+artistes = acteurs + realisateurs
+realisateurs = artistes - acteurs
+realisateurs = realisateur FROM Films EXCEPT aid FROM Roles
